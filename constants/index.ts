8a3e37e7d41207bebc779366be424e8ed58b11ca
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-05-17 10:53:09 +0530

    added constants

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordigs",
+    imgUrl: "/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/home.svg",
+  },
+];
