5ff038505c2c2f875b685b94242dd57867d3bdcb
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-05-20 12:05:39 +0530

    added more color types

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,20 @@ module.exports = {
         blue: {
           1: "#0E78F9",
         },
+        sky: {
+          1: "#C9DDFF",
+          2: "#ECF0FF",
+          3: "#F5FCFF",
+        },
+        orange: {
+          1: "#FF742E",
+        },
+        purple: {
+          1: "#830EF9",
+        },
+        yellow: {
+          1: "#F9A90E",
+        },
       },
       backgroundImage: {
         hero: "url('/images/hero-background.png')",
